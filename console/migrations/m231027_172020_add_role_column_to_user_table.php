ee7f60249a8cd0749956cff937aa86a30ddecb50
@vokulakov <vokulakov01@gmail.com>
2023-10-27 20:25:03 +0300

    API. Создана миграция для хранения роли пользователя в таблице пользователей.

---
+++ console/migrations/m231027_172020_add_role_column_to_user_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding columns to table `{{%user}}`.
+ */
+class m231027_172020_add_role_column_to_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%user}}', 'role', $this->smallInteger()->defaultValue(10));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%user}}', 'role');
+    }
+}
